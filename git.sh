31e81aac84a28816ab4c2e0b10dca4a9d872e256
bijanbina <bijanbina@gmail.com>
2020-01-04 10:44:48 +0330

    merge git push & pull

---
+++ git.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+source setup_variables.sh
+
+echo "-------------------------------------"
+echo "0 - Pull from Git"
+echo "1 - Push to Git"
+echo "-------------------------------------"
+read response
+
+if [ "$response" = "0" ]; then
+	git pull origin master
+	printf "Copy PetaLinux Files from git?[y/N]: "
+	read response
+
+	if [ "$response" = "y" ]; then
+		cp -R Petalinux/pna-iio/ "$PETALINUX_INSTALL_DIR/$PETALINUX_PROJECT/project-spec/meta-user/recipes-apps/pna-iio/"
+		cp -R Petalinux/pnadmc/ "$PETALINUX_INSTALL_DIR/$PETALINUX_PROJECT/project-spec/meta-user/recipes-modules/pnadmc/"
+	fi
+
+	printf "Copy SDK Files from git?[y/N]: "
+	read response
+
+	if [ "$response" = "y" ]; then
+		cp -R SDK/*.c "$SDK_PROJECT/"
+		cp -R SDK/*.h "$SDK_PROJECT/"
+	fi
+elif [ "$response" = "1" ]; then
+	printf "Copy PetaLinux Files to git?[y/N]: "
+	read response
+
+	if [ "$response" = "y" ]; then
+		cp -R "$PETALINUX_INSTALL_DIR/$PETALINUX_PROJECT/project-spec/meta-user/recipes-apps/pna-iio/" Petalinux/
+		cp -R "$PETALINUX_INSTALL_DIR/$PETALINUX_PROJECT/project-spec/meta-user/recipes-modules/pnadmc/" Petalinux/
+	fi
+
+	printf "Copy SDK Files to git?[y/N]: "
+	read response
+
+	if [ "$response" = "y" ]; then
+		rm SDK/*.c
+		rm SDK/*.h
+		cp -R $SDK_PROJECT/*.h SDK/
+		cp -R $SDK_PROJECT/*.c SDK/
+	fi
+
+	git status
+
+	printf "Enter git commit comment: "
+	read response
+
+	git commit -a -m "$response"
+
+	printf "Git push?[y/N]: "
+	read response
+	if [ "$response" = "y" ]; then
+		git push origin master
+	fi
+fi
